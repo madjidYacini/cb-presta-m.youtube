bf67db2f8c73fd8b260650c7fe6efc9a6e537e79
madjid yacini <madjid.yacini@gmail.com>
2018-05-08 01:46:32 +0200

    search

--- screens/video.js
+++ screens/video.js
@@ -16,28 +16,25 @@ import { WebView } from 'react-native';
 
 
 export default class Video extends React.Component {
-                static navigationOptions = { headerStyle: { backgroundColor: '#f42627' }, 
-                
-                      headerRight:(
+                 static navigationOptions = ({ navigation }) => {
+                   const { state, setParams, navigate } = navigation;
+                   return { headerStyle: { backgroundColor: "#f42627" }, headerLeft: <TouchableOpacity>
+                         <Image source={require("../assets/logo.png")} style={{ marginLeft: 10 }} />
+                       </TouchableOpacity>, headerRight: <View style={{ flexDirection: "row", marginRight: 30 }}>
                          
-                      <View style={{ flexDirection: 'row', marginRight: 30 }}>
+                         <TouchableOpacity style={{ paddingHorizontal: 5 }} onPress={() => navigation.navigate("like", {})}>
+                           <Image style={{ width: 20, height: 20 }} source={require("../assets/love.png")} />
+                         </TouchableOpacity>
+
                        
-                      <TouchableOpacity style={{ paddingHorizontal: 5 }}>
-                         <Icon name="videocam" size={25} color="#FFF" />
-                       </TouchableOpacity>
-                       <TouchableOpacity style={{ paddingHorizontal: 5 }}>
-                         <Icon name="search" size={25} color="#FFF" />
-                       </TouchableOpacity>
-                       
-                       <TouchableOpacity style={{ paddingHorizontal: 5 }}>
-                         <Icon name="account-circle" size={25} color="#FFF" />
-                       </TouchableOpacity>
-                       
-                        <TouchableOpacity style={{ paddingHorizontal: 5 }}>
-                         <Icon name="account-circle" size={25} color="#FFF" />
-                       </TouchableOpacity>
-                      </View> )
-                     };
+                         <TouchableOpacity style={{ paddingHorizontal: 5 }} onPress={() => navigation.navigate("settings", {})}>
+                           <Image style={{ width: 20, height: 20 }} source={require("../assets/settings.png")} />
+                         </TouchableOpacity>
+                         <TouchableOpacity style={{ paddingHorizontal: 5 }}>
+                           <Image style={{ width: 20, height: 20 }} source={require("../assets/user.png")} />
+                         </TouchableOpacity>
+                       </View> };
+                 };
                  render() {
                    const { params } = this.props.navigation.state;
                    return <WebView source={{ uri: `https://www.youtube.com/watch?v=${params.url}` }} style={{ marginTop: 20 }} />;
